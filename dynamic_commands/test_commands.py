8d2bea1ef064e17ccffe173051da4b96a01522d1
wiwa5607 <wiwa5607@colorado.edu>
2019-11-12 14:39:51 -0700

    Dynamic Column Creation

--- dynamic_commands/test_commands.py
+++ dynamic_commands/test_commands.py
@@ -11,52 +11,53 @@ obd.logger.setLevel(obd.logging.DEBUG)
 # * ask for userid
 # car_id = input("Plase input your userid from the website so we can upload your information to your account:")
 # * make database connection
-dbconn = psycopg2.connect("host=198.23.146.166 dbname=postgres user=postgres")
+# host=198.23.146.166  password=Sweden77
+dbconn = psycopg2.connect("host=198.23.146.166 dbname=car user=postgres")
 cur = dbconn.cursor()
 # * make car connection:
 car = obd.OBD()  # * auto-connects to USB or RF port
-dbtable = "testcar0"
+dbtable = "testing_dynamic_columns"
 # print(obd.commands.MAF)
 command = []
 test_dict = obd.commands.__dict__
+# for key, i in test_dict.items():
+#     command.append((key, test_dict[key]))
+# print(*command[0][1], sep="\n \n")
 # ! async experiment
 # speed = obd.OBDCommand.SPEED
 # RPM = obd.OBDCommand.RPM
 # etemp = obd.OBDCommand.COOLANT_TEMP
 # fuel = obd.OBDCommand.FUEL_LEVEL
-speed = ['SPEED KMPH', '45.0 kph']
-RPM = ['RPM', '1400 revolutions_per_minute']
-etemp = ['Coolant Temp', '13 degC']
-fuel = ['FUEL LEVEL', '85.00000 %']
-time = ['TIME', datetime.datetime.now()]
-testmaf = ['MAF', '0.25 gpm']
-temp = [speed, RPM, etemp, fuel, testmaf]
-# columns = ['TIME', 'SPEED', 'RPM', 'TEMP', 'FUEL', 'MAF']
-# results = [str(time).rsplit(' ', 1)[0], str(speed).rsplit(' ', 1)[0], str(RPM).rsplit(' ', 1)[0], str(etemp).rsplit(' ', 1)[0], str(fuel).rsplit(' ', 1)[0],
-#            str(testmaf).rsplit(' ', 1)[0]]
-columns = ["time"]
-results = [datetime.datetime.now()]
-for i in range(0, len(temp)):
-    res = temp[i][1]
-    description = str(temp[i][0])
-    if(res != 'None'):
-        columns.append(description)
-        results.append(str(res).rsplit(' ', 1)[0])
-# * testing dynamic column generation
-for i in range(0, len(columns)):
-    data = columns[i]
-    cur.execute("select exists(select 1 from information_schema.columns where table_name=%s and column_name=%s);",
-                (dbtable, data))
-    test = cur.fetchone()[0]
-    if(not test):
-        cur.execute("alter table %s add column \"%s\" float",
-                    (AsIs(dbtable), AsIs(data)))
+# speed = ['SPEED KMPH', '45.0 kph']
+# RPM = ['RPM', '1400 revolutions_per_minute']
+# etemp = ['Coolant Temp', '13 degC']
+# fuel = ['FUEL LEVEL', '85.00000 %']
+# time = ['TIME', datetime.datetime.now()]
+# testmaf = ['MAF', '0.25 gpm']
+# temp = [speed, RPM, etemp, fuel, testmaf]
+# columns = ["time"]
+# results = [datetime.datetime.now()]
+# for i in range(0, len(temp)):
+#     res = temp[i][1]
+#     description = str(temp[i][0])
+#     if(res != 'None'):
+#         columns.append(description)
+#         results.append(str(res).rsplit(' ', 1)[0])
+# # * testing dynamic column generation
+# for i in range(0, len(columns)):
+#     data = columns[i]
+#     cur.execute("select exists(select 1 from information_schema.columns where table_name=%s and column_name=%s);",
+#                 (dbtable, data))
+#     test = cur.fetchone()[0]
+#     if(not test):
+#         cur.execute("alter table %s add column \"%s\" VARCHAR(200)",
+#                     (AsIs(dbtable), AsIs(data)))
 # * final insertion
 # print(sql.SQL(', ').join(sql.Identifier(n) for n in results).as_string(dbconn))
-q1 = sql.SQL("insert into testcar0 values ({})").format(
-    sql.SQL(', ').join(sql.Placeholder() * len(results)))
+# q1 = sql.SQL("insert into {0} values ({1})").format(sql.Identifier(dbtable),
+#                                                     sql.SQL(', ').join(sql.Placeholder() * len(results)))
 # print(q1.as_string(dbconn))
-cur.execute(q1, results)
+# cur.execute(q1, results)
 
 # res = {key: test_dict[key] for key in test_dict.keys()
 #                                & {'MAF'}}
@@ -65,19 +66,19 @@ if(car.status() == OBDStatus.NOT_CONNECTED):
     print("Failed OBD-II Query, please try again")
 else:
     # * VIN experiment
-    vin = obd.OBDCommand.VIN
-    print(vin)
+    # vin = obd.OBDCommand.VIN
+    # print(vin)
+    # * column names
+    columns = ["time"]
+    # * column values
+    results = [datetime.datetime.now()]
     for key, i in test_dict.items():
         # print(key, test_dict[key])
         command.append((key, test_dict[key]))
     # command = tuple(command)
     # print(command[0])
     temp2 = command[0][1][1]
-    # print(temp2)
-    # * column names
-    columns = ["time"]
-    # * column values
-    results = [datetime.datetime.now()]
+    print(command[0][1][1][1])
     for i in range(0, len(temp2)):
         # print("Temp2 is", temp2[1][i])
         # print("Type is", type(temp2[1][i]))
@@ -88,23 +89,29 @@ else:
         # print("Final command is")
         # print(description, res)
         if(res != 'None'):
-            columns.append(description)
-            results.append(str(res).rsplit(' ', 1)[0])
-    temp2 = command[0][1][6]
-    for i in range(0, len(temp2)):
-        res = str((car.query(temp2[i])).value)
-        description = str(temp2[i])
-        if(res != 'None'):
-            columns.append(description)
+            columns.append(description.rsplit(': ',1)[1])
             results.append(str(res).rsplit(' ', 1)[0])
     temp2 = command[0][1][2]
     for i in range(0, len(temp2)):
         res = str((car.query(temp2[i])).value)
         description = str(temp2[i])
         if(res != 'None'):
-            columns.append(description)
+            columns.append(description.rsplit(': ',1)[1])
             results.append(str(res).rsplit(' ', 1)[0])
-    # print(temp2)
+    temp2 = command[0][1][6]
+    for i in range(0, len(temp2)):
+        res = str((car.query(temp2[i])).value)
+        description = str(temp2[i])
+        if(res != 'None'):
+            columns.append(description.rsplit(': ',1)[1])
+            results.append(str(res).rsplit(' ', 1)[0])
+    # temp2 = command[0][1][9]
+    # for i in range(0, len(temp2)):
+    #     res = str((car.query(temp2[i])).value)
+    #     description = str(temp2[i])
+    #     if(res != 'None'):
+    #         columns.append(description)
+    #         results.append(str(res).rsplit(' ', 1)[0])
     # * length checking for all arrays
     if(len(columns) != len(results)):
         print("Results error")
@@ -118,12 +125,14 @@ else:
                         (dbtable, data))
             test = cur.fetchone()[0]
             if(not test):
-                cur.execute("alter table %s add column \"%s\" float",
+                data = data.replace("\"", "\'")
+                cur.execute("alter table %s add column \"%s\" VARCHAR(2000)",
                             (AsIs(dbtable), AsIs(data)))
         # * final insertion
-        q1 = sql.SQL("insert into testcar0 values ({})").format(
-            sql.SQL(', ').join(sql.Placeholder() * len(results)))
+        q1 = sql.SQL("insert into {0} values ({1})").format(sql.Identifier(dbtable),
+                                                            sql.SQL(', ').join(sql.Placeholder() * len(results)))
         cur.execute(q1, results)
+        print("Successful Read")
 if(dbconn):
     cur.close()
     dbconn.commit()
